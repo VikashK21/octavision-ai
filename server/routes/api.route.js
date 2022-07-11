8cfc822e16513f8729d5214d4623b48ba5fbac2d
VikashK21 <vikash21@navgurukul.org>
2022-07-11 21:10:15 +0530

    just a changed

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const ContrlT = new (require("../controller/cusmoter.controller"))();
+const ContrlT = new (require("../controllers/cusmoter.controller"))();
 
 router.get("/", async (req, res, next) => {
   res.send({ message: "Ok api is working 🚀" });
 });
 
 router.get("/customers", ContrlT.allData);
-router.post('/customer', ContrlT.CreateData);
-router.get('/customer/:id', ContrlT.DataById)
+router.post("/customer", ContrlT.CreateData);
+router.get("/customer/:id", ContrlT.DataById);
 
 module.exports = router;
