5b7cf22bfb08000f111644bdb7c61440f93bbb6a
VikashK21 <vikash21@navgurukul.org>
2022-08-06 21:09:58 +0530

    added the swagger documentations with some modules and middlewares

--- server/app.js
+++ server/app.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const createError = require('http-errors');
-const morgan = require('morgan');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const createError = require("http-errors");
+const morgan = require("morgan");
+require("dotenv").config();
+const cors = require("cors");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
+app.get("/", async (req, res, next) => {
+  res.send({ message: "Awesome it works 🐻" });
 });
 
-app.use('/api', require('./routes/api.route'));
+const swaggerOptions = require("./swagger.json");
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use("/api", require("./routes/api.route"));
+app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use((req, res, next) => {
   next(createError.NotFound());
--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -6,7 +6,74 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/customers", ContrlT.allData);
+/**
+ * @swagger
+ * /api/customers:
+ *  get:
+ *    description: Will get all the customers data.
+ *    responses:
+ *      '200':
+ *        description: Success
+ *      '400':
+ *        description: Error message
+ */
+
 router.post("/customer", ContrlT.CreateData);
+/**
+ * @swagger
+ * /api/customer:
+ *  post:
+ *    description: Used to create an entry of customer's data.
+ *    responses:
+ *      '201': 
+ *        description: Object
+ *      '400': 
+ *        description: Error message
+ *    produces:
+ *      - application/json
+ *    parameters: 
+ *      - name: name
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: email
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: password
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: phone_num
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: description
+ *        in: formData
+ *        required: true
+ *        type: string
+ */
+
 router.get("/customer/:id", ContrlT.DataById);
+/**
+ * @swagger
+ * paths:
+ *  /api/customer/{id}:
+ *     get:
+ *       parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: integer
+ *            minimum: 1
+ *          description: The customer ID
+ *       description: Will get all the customers data.
+ *       responses:
+ *         '200':
+ *           description: Success
+ *         '400':
+ *           description: Error message
+ */
 
 module.exports = router;
